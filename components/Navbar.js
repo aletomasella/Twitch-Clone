3ca10dc6b28c842874648d5829fe0c0f540b0a58
Alejandro Tomasella <aletomasella09@gmail.com>
2022-06-28 17:27:59 -0300

    Working on the NextAuth

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,7 @@ import twitchLogo from "../public/assets/twitchLogo.png";
 import { Menu, Transition } from "@headlessui/react";
 import { BsThreeDotsVertical, BsSearch, BsPerson } from "react-icons/bs";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -14,6 +15,10 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleNavbar = () => setIsOpen(!isOpen);
+
+  const { data: session } = useSession();
+
+  console.log(session);
 
   return (
     <>
@@ -119,14 +124,93 @@ const Navbar = () => {
         </div>
         {/* Right Size */}
         <div className="hidden md:flex grow items-center justify-end">
-          <div className="flex items-center">
-            <Link href={"/account"}>
-              <button className="px-4 py-[6px] rounded-lg font-bold bg-[#9147ff] mr-2">
-                Account
-              </button>
-            </Link>
-            <BsPerson size={30} />
-          </div>
+          {session ? (
+            <div className="flex items-center">
+              <Link href={"/account"}>
+                <div>
+                  <p className="pr-4 cursor-pointer">
+                    Welcome, {session.user.name}
+                  </p>
+                </div>
+              </Link>
+              <Menu as="div" className="relative text-left">
+                <div className="flex">
+                  <Menu.Button>
+                    <BsThreeDotsVertical size={20} />
+                  </Menu.Button>
+                </div>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-[#0e0e10] ring-1 ring-white ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="/account"
+                            className={classNames(
+                              active
+                                ? "bg-gray-500 text-gray-100"
+                                : "text-gray-200",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Account
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-500 text-gray-100"
+                                : "text-gray-200",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            Support
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active
+                                ? "bg-gray-500 text-gray-100"
+                                : "text-gray-200",
+                              "block px-4 py-2 text-sm"
+                            )}
+                          >
+                            License
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+            </div>
+          ) : (
+            <div className="flex items-center">
+              <Link href={"/account"}>
+                <button className="px-4 py-[6px] rounded-lg font-bold bg-[#9147ff] mr-2">
+                  Account
+                </button>
+              </Link>
+              <BsPerson size={30} />
+            </div>
+          )}
         </div>
         {/* Hambuger Menu */}
         <div
--- pages/account.js
+++ pages/account.js
@@ -1,14 +1,32 @@
 import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import Image from "next/image";
 
 export default function account() {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <div className="pt-[100px] flex flex-col max-w-[400px] w-full p-4">
+          <h2 className="text-2xl font-bold">Welcome {session.user.name}</h2>
+          <p className="p-4">Signed in as {session.user.email}</p>
+          <div className="pb-4 m-auto">
+            <Image
+              className="rounded-full"
+              src={session.user.image}
+              alt="User Photo"
+              width={100}
+              height={100}
+            />
+          </div>
+          <button
+            className="flex items-center justify-center p-3 bg-gray-600 border-gray-600 rounded-sm"
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+        </div>
       </>
     );
   }
@@ -21,8 +39,15 @@ export default function account() {
           onClick={() => signIn()}
           className="flex items-center justify-center p-3 bg-gray-600 border-gray-600 my-2"
         >
-          <FaGithub className="2" /> Sign in with{" "}
+          <FaGithub className="2 mx-2" /> Sign in with{" "}
           <span className="font-bold pl-1">GitHub</span>{" "}
+        </button>
+        <button
+          onClick={() => signIn()}
+          className="flex items-center justify-center p-3 bg-blue-600 border-blue-600 my-2"
+        >
+          <FaGoogle className="2 mx-2" /> Sign in with{" "}
+          <span className="font-bold pl-1">Google</span>{" "}
         </button>
       </div>
     </>
